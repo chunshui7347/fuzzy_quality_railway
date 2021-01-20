0a58d85bf289e6acf905b6178f0b1a56e8fe89da
Tan Chun Shui <chunshui.tan@capitalbay.com.my>
2021-01-20 20:19:43 +0800

    fix_typo

--- main_part2.py
+++ main_part2.py
@@ -207,7 +207,7 @@ for i in range(len(file)):
     rate_system.input['price'] = price_list[i]
     rate_system.input['frequency'] = frequency_list[i]
 
-    # Defuzzification using the Centre of Gravity method, and output a risk toelance score#
+    # defuzzification using centre of gravity"
     try:
         rate_system.compute()
         score = rate_system.output['quality']
